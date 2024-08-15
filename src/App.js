d151a176fcc3b5b3e574410803f27b7432befe4d
chaddjong <richardicad85@gmail.com>
2024-08-15 22:57:18 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
+/* eslint-disable prettier/prettier */
+
+import {StyleSheet, Alert, Linking, BackHandler} from 'react-native';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from './routes';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
@@ -12,19 +14,51 @@ import {AuthProvider} from './context/AuthContext';
 import {AdsProvider} from './context/AdsContext';
 import {MPDigitalProvider} from './context/MPDigitalContext';
 import {TokenProvider} from './context/TokenContext';
+import VersionCheck from 'react-native-version-check';
+
 GoogleSignin.configure({
   webClientId:
     '782626479856-89khocqerprpe29tscrpvdn5vb8ghan0.apps.googleusercontent.com',
 });
 
 const App = () => {
-  const storeSession = async detail => {
+  const latestVersion = "2.1.9"; // Define the latest version here
+  const playStoreUrl = "https://play.google.com/store/apps/details?id=com.mp.manadopost&pcampaignid=web_share"; // Play Store URL
+
+  const storeSession = async (detail) => {
     try {
       await EncryptedStorage.setItem('detail', JSON.stringify(detail));
     } catch (error) {
       console.log(error);
     }
   };
+
+  const checkForSpecificVersion = async () => {
+    try {
+      const currentVersion = await VersionCheck.getCurrentVersion();
+
+      if (currentVersion !== latestVersion) {
+        Alert.alert(
+          "Update Required",
+          `You're using version ${currentVersion}. Please update to the latest version in the Play Store.`,
+          [
+            { 
+              text: "Update Now", 
+              onPress: () => {
+                // Linking.openURL(playStoreUrl)
+                //   .catch(err => console.error("Failed to open URL", err));
+                BackHandler.exitApp(); // Close the app
+              },
+            },
+          ],
+          { cancelable: false } // Prevent the user from dismissing the alert without updating
+        );
+      }
+    } catch (error) {
+      console.log("Error checking version:", error);
+    }
+  };
+
   useEffect(() => {
     axios
       .post(promediaAuth, authData, authConfig)
@@ -35,6 +69,8 @@ const App = () => {
       .catch(error => {
         console.log(error);
       });
+
+    checkForSpecificVersion(); // Check if the current version matches the target version when the app starts
   }, []);
 
   return (
