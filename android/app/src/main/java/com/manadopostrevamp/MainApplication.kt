909ca40a2a90c668051f1f6432d3d2a45ff1354b
jck-y <s2200582@student.unklab.ac.id>
2024-08-16 09:28:31 +0800

    Update: GooglePlay Billing

--- android/app/src/main/java/com/manadopostrevamp/MainApplication.kt
+++ android/app/src/main/java/com/manadopostrevamp/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.mp.manadopost
 
 import android.app.Application
+import com.dooboolab.rniap.RNIapPackage // Import ini sudah benar
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -18,8 +19,8 @@ class MainApplication : Application(), ReactApplication {
         object : DefaultReactNativeHost(this) {
             override fun getPackages(): List<ReactPackage> =
                 PackageList(this).packages.apply {
-                    // Packages that cannot be autolinked yet can be added manually here, for example:
-                    // add(MyReactNativePackage())
+                    // Remove manual addition of RNIapPackage
+                    // add(RNIapPackage())
                 }
 
             override fun getJSMainModuleName(): String = "index"
